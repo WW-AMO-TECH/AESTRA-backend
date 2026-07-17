1524508d8615eeab4a1cc0f1d9a6ac56987697dc
Richmond Chinelo <richmondchineloanne@gmail.com>
2026-07-17 20:04:05 +0100

    Modified cors.php again 2

--- config/cors.php
+++ config/cors.php
@@ -20,9 +20,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://aestra-frontend.vercel.app',
-    ],
+    'allowed_origins' => ['https://aestra-frontend.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
